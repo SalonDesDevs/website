58d37dd57211a90b39614490d983aade3fe1dcbf
FliiFe <theophile.cailliau@gmail.com>
2017-08-30 20:36:51 +0200

    Added footer

---
+++ src/components/Footer/FooterSection.vue
@@ -0,0 +1,144 @@
+<template>
+    <footer>
+        <h3>Retrouvez nous !</h3>
+        <div class="icons-container">
+            <a target="_blank" href="https://github.com/SalonDesDevs">
+                <svg class="github" aria-labelledby="title" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><title>GitHub icon</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
+            </a>
+            <a target="_blank" href="https://gitlab.com/SalonDesDevs">
+                <svg class="gitlab" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path d="M15.97 9.058l-.895-2.756L13.3.842c-.09-.282-.488-.282-.58 0L10.946 6.3H5.054L3.28.842C3.188.56 2.79.56 2.7.84L.924 6.3.03 9.058c-.082.25.008.526.22.682L8 15.37l7.75-5.63c.212-.156.302-.43.22-.682"/></svg>
+            </a>
+            <a target="_blank" href="https://salondesdevs.io/discord">
+                <svg class="discord" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path d="M13.487-.002c.936 0 1.693.758 1.738 1.65v14.35l-1.783-1.515-.98-.892-1.07-.93.446 1.47h-9.36c-.935 0-1.693-.71-1.693-1.65V1.65C.785.76 1.543 0 2.48 0h11zm-4.08 3.788h-.02l-.134.134c1.382.4 2.05 1.025 2.05 1.025-.89-.446-1.694-.668-2.496-.758-.58-.09-1.16-.044-1.65 0h-.132c-.312 0-.98.134-1.872.49-.312.134-.49.223-.49.223s.668-.668 2.14-1.025l-.09-.09s-1.115-.043-2.318.848c0 0-1.203 2.095-1.203 4.68 0 0 .668 1.158 2.495 1.203 0 0 .268-.356.535-.668-1.025-.312-1.426-.936-1.426-.936s.09.044.223.133h.04c.02 0 .03.01.04.02v.005c.01.01.02.02.04.02.22.09.44.178.62.267.31.134.71.268 1.2.357.62.09 1.33.134 2.14 0 .4-.09.8-.178 1.2-.357.26-.133.58-.267.93-.49 0 0-.4.624-1.47.936.22.312.53.668.53.668 1.83-.04 2.54-1.2 2.58-1.15 0-2.58-1.21-4.68-1.21-4.68-1.09-.81-2.11-.84-2.29-.84zm.113 2.942c.468 0 .847.4.847.89 0 .493-.38.892-.847.892-.467 0-.846-.4-.846-.89 0-.493.38-.892.846-.892zm-3.03 0c.467 0 .846.4.846.89 0 .493-.38.892-.846.892-.468 0-.847-.4-.847-.89 0-.493.38-.892.847-.892z"/></svg>
+            </a>
+            <a target="_blank" href="https://www.youtube.com/channel/UCj0cYs6HJ6jRPLABDITK8pg">
+                <svg class="youtube" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path d="M0 7.345c0-1.294.16-2.59.16-2.59s.156-1.1.636-1.587c.608-.637 1.408-.617 1.764-.684C3.84 2.36 8 2.324 8 2.324s3.362.004 5.6.166c.314.038.996.04 1.604.678.48.486.636 1.588.636 1.588S16 6.05 16 7.346v1.258c0 1.296-.16 2.59-.16 2.59s-.156 1.102-.636 1.588c-.608.638-1.29.64-1.604.678-2.238.162-5.6.166-5.6.166s-4.16-.037-5.44-.16c-.356-.067-1.156-.047-1.764-.684-.48-.487-.636-1.587-.636-1.587S0 9.9 0 8.605v-1.26zm6.348 2.73V5.58l4.323 2.255-4.32 2.24z"/></svg>
+            </a>
+            <a target="_blank" href="https://twitter.com/salondesdevs_io">
+                <svg class="twitter" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path d="M16 3.038c-.59.26-1.22.437-1.885.517.677-.407 1.198-1.05 1.443-1.816-.634.37-1.337.64-2.085.79-.598-.64-1.45-1.04-2.396-1.04-1.812 0-3.282 1.47-3.282 3.28 0 .26.03.51.085.75-2.728-.13-5.147-1.44-6.766-3.42C.83 2.58.67 3.14.67 3.75c0 1.14.58 2.143 1.46 2.732-.538-.017-1.045-.165-1.487-.41v.04c0 1.59 1.13 2.918 2.633 3.22-.276.074-.566.114-.865.114-.21 0-.41-.02-.61-.058.42 1.304 1.63 2.253 3.07 2.28-1.12.88-2.54 1.404-4.07 1.404-.26 0-.52-.015-.78-.045 1.46.93 3.18 1.474 5.04 1.474 6.04 0 9.34-5 9.34-9.33 0-.14 0-.28-.01-.42.64-.46 1.2-1.04 1.64-1.7z" fill-rule="nonzero"/></svg>
+            </a>
+        </div>
+        <div class="footer-separator"></div>
+        <div class="bottomline">
+            <p class="footer-about">Design par <a target="_blank" href="https://twitter.com/valentin_SLD_">valmax72</a></p>
+            <p class="copyright">Copyright Salon des Développeurs.</p>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'footer-section',
+}
+</script>
+
+<style scoped>
+footer {
+    background-color: #262626;
+    color: #fafafa;
+    width: 100%;
+}
+
+h3 {
+    margin: 0;
+    text-align: center;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    font-weight: normal;
+    font-size: 28px;
+}
+
+div.icons-container {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+svg {
+    width: 60px;
+    fill: #888;
+    margin: 20px;
+}
+
+.github:hover {
+    fill: white;
+}
+
+.discord:hover {
+    fill: #7289da;
+}
+
+.gitlab:hover {
+    fill: #e24329;
+}
+
+.youtube:hover {
+    fill: #cd201f;
+}
+
+.twitter:hover {
+    fill: #1da1f2;
+}
+
+div.footer-separator {
+    width: calc(100% - 130px);
+    margin: 0 65px;
+    height: 3px;
+    background-color: #444;
+    border-radius: 5px;
+}
+
+p.copyright {
+    margin: 0;
+    right: 0;
+    padding-right: 65px;
+    padding-top: 5px;
+    font-size: 17px;
+    color: #999;
+    padding-bottom: 10px;
+    text-align: right;
+}
+
+div.bottomline {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+
+p.footer-about {
+    margin: 0;
+    right: 0;
+    padding-left: 65px;
+    padding-top: 5px;
+    font-size: 17px;
+    color: #999;
+    padding-bottom: 10px;
+}
+
+p > a, p > a:visited {
+    color: #999;
+    font-size: 17px;
+    text-decoration: none;
+    border-bottom: 2px solid #999;
+}
+
+p > a:hover {
+    text-decoration: none;
+    border-bottom: none;
+}
+
+@media (max-width: 600px) {
+    div.bottomline {
+        flex-direction: column-reverse;
+        align-items: center;
+    }
+
+    p {
+        padding-left: 0 !important;
+        padding-right: 0 !important;
+    }
+}
+</style>
