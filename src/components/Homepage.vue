ec2a9cb389711e337ce3f3999b824d7330448773
FliiFe <theophile.cailliau@gmail.com>
2017-08-30 20:37:15 +0200

    Added footer and temporary placeholder

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -3,6 +3,8 @@
         <homepage-header></homepage-header>
         <last-articles></last-articles>
         <discord-presentation></discord-presentation>
+        <div class="placeholder-twitter"></div>
+        <footer-section></footer-section>
     </div>
 </template>
 
@@ -10,13 +12,15 @@
 import HomepageHeader from './Header/Homepage/Homepage.vue'
 import LastArticles from './Articles/LastArticles.vue'
 import DiscordPresentation from './DiscordPresentation/DiscordPresentation.vue'
+import FooterSection from './Footer/FooterSection.vue'
 
 export default {
     name: 'homepage',
     components: {
         HomepageHeader,
         LastArticles,
-        DiscordPresentation
+        DiscordPresentation,
+        FooterSection
     }
 }
 </script>
@@ -24,5 +28,9 @@ export default {
 <style scoped>
 div#app {
     overflow-x: hidden;
+}
+
+div.placeholder-twitter {
+    height: 800px;
 }
 </style>
