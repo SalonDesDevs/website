3d10b6bb31cd5c15daaa8848a7005740182c212f
FliiFe <theophile.cailliau@gmail.com>
2017-08-31 14:32:56 +0200

    Added vue-router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+
+Vue.use(VueRouter);
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+    el: '#app',
+    render: h => h(App)
+});
