f2f5ca4d2e30185fc4d3c9f5dd63047d4cd47042
FliiFe <theophile.cailliau@gmail.com>
2017-09-03 12:42:48 +0200

    Show loading screen

--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="articles">
         <articles-header></articles-header>
+        <loader></loader>
         <last-articles-full-list></last-articles-full-list>
         <footer-section></footer-section>
     </div>
@@ -10,13 +11,15 @@
 import ArticlesHeader from './Header/Articles/Articles.vue';
 import FooterSection from './Footer/FooterSection.vue';
 import LastArticlesFullList from './Articles/LastArticlesFullList.vue';
+import Loader from './Loader/Loader.vue';
 
 export default {
     name: 'articles',
     components: {
         ArticlesHeader,
         FooterSection,
-        LastArticlesFullList
+        LastArticlesFullList,
+        Loader
     },
     head: {
         title: {
@@ -27,7 +30,7 @@ export default {
             { property: 'og:url', content: window.location },
             { name: 'twitter:title', content: 'Derniers Articles - Salon des développeurs' },
             { name: 'twitter:description', content: 'Articles écrits par la communauté, sur tout et n\'importe quoi !' },
-            { name: 'description', content: 'Articles écrits par la communauté, sur tout et n\'importe quoi !' }
+            { id: 'meta-desc', name: 'description', content: 'Articles écrits par la communauté, sur tout et n\'importe quoi !' }
         ]
     }
 };
